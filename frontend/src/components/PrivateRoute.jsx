a905e3b31e90179823f80df9dc1e0548af11d027
Ranjit singh <adityarajsinghshaktawat96@gmail.com>
2024-09-30 10:53:41 +0530

    my-ecommerce-pro

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import Navbar from './Home/Navbar'
+function PrivateRoute({allowrole}) {
+    const token = localStorage.getItem('token')
+    const role = localStorage.getItem('role')
+
+    if(!token){
+        return <Navigate to='/login' />
+    }
+  
+ if(!allowrole.includes(role)){
+    return <Navigate to="/login"/>
+ }
+ return  (
+    <>
+
+    <Outlet/></>
+    
+ )
+ 
+   
+
+ 
+}
+
+export default PrivateRoute
